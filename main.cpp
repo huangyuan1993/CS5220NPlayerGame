f1a0b5b5c73e705cc9109c51cd488c220dc3830c
huangyuan1993 <huangyuan1993@163.com>
2015-12-12 21:50:50 -0500

    v2,1

--- main.cpp
+++ main.cpp
@@ -20,9 +20,10 @@ int main(int argc, char** argv)
 	struct timeval startTime, endTime;
     long totalTime;
     gettimeofday(&startTime, NULL);
-    
-
-
+	int n_id, n_sz; 
+	MPI_Init(&argc, &argv); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &n_id); 
+	MPI_Comm_size(MPI_COMM_WORLD, &n_sz);
 	int n = 0;
 	char ch[100];
 	fscanf(ifp, "%d", &n);
@@ -78,151 +79,174 @@ int main(int argc, char** argv)
 	char num[1000];
 	//FILE * ofp = fopen("outp.txt", "w");
 	//fprintf(ofp, "%d\n",num_eq);
-	for(int i=0;i<n;++i){
-		int done = 0;
-		eq0.clear();
-		eq0 += "r";
-		eq0+=to_string(i+1);
-		eq0+="1";
-		for(int k=0;k<dimension[n]/kn[i];++k) {
-			int smallk =k%dimension[i];
-			int curk = (k-smallk)*kn[i]+smallk;
-			int idc = 0;
-			if(util[curk][i]>=0 ){
-				eq0+=" + ";
-			}
-			
-			eq0+=to_string(util[curk][i]);
-			for(int w=n-1;w>=0;--w){
-				currentIndex[w] = curk/dimension[w];
-				curk = curk%dimension[w];
-				if(w==i)continue;
-				eq0+=" * ";
-				eq0+="x";		
-				eq0+=to_string(w+1);
-				eq0+=to_string(currentIndex[w]+1);
-			}
-			
-		}
+	int combnum=1,shiftcnt=0,shiftmask;
+	int* combmask=(int*)malloc(*sizeof(int));
+	int* shiftall=(int*)malloc(*sizeof(int));
+	for(int comb=0;comb<n;++comb)
+	{
+		shiftmask = (1<<kn[comb])-1;
+		combmask[comb]=shiftmask<<shiftcnt;
+		shiftall[comb]=shiftcnt;
+		shiftcnt+=kn[comb];
 		
-		for(int j=1;j<kn[i];++j){
-			eqj.clear();
-			//using r
-			eqj += "r";
-			
-			eqj+=to_string(i+1);
-			
-			eqj+=to_string(j+1);
-			//using r
+	}
+	combnum<<=shiftcnt;
+	for(count=0;count<combnum;++count;){
+		
+		int valid = 1;
+		for(int cnt=0;cnt<n;++cnt){
+			int curmask = combmask(cnt)&count);
+			if(curmask==0)count+=(1<<shiftall[n]);
+		}
+		if(count%n_sz!=n_id)continue;
+		for(int i=0;i<n;++i){
+			int done = 0;
+			eq0.clear();
+			eq0 += "r";
+			eq0+=to_string(i+1);
+			eq0+="1";
 			for(int k=0;k<dimension[n]/kn[i];++k) {
 				int smallk =k%dimension[i];
-				int curk = (k-smallk)*kn[i] + smallk + dimension[i]*j;
+				int curk = (k-smallk)*kn[i]+smallk;
 				int idc = 0;
-				if(util[curk][i]>=0){//k=1
-					eqj+=" + ";
+				if(util[curk][i]>=0 ){
+					eq0+=" + ";
 				}
 				
-				eqj+=to_string(util[curk][i]);
+				eq0+=to_string(util[curk][i]);
 				for(int w=n-1;w>=0;--w){
 					currentIndex[w] = curk/dimension[w];
 					curk = curk%dimension[w];
 					if(w==i)continue;
-					eqj+=" * ";
-					eqj+="x";
-					
-					eqj+=to_string(w+1);
-					
-					eqj+=to_string(currentIndex[w]+1);;
-					
+					eq0+=" * ";
+					eq0+="x";		
+					eq0+=to_string(w+1);
+					eq0+=to_string(currentIndex[w]+1);
 				}
 				
-			}	
-			eqj += " - ( "+ eq0 + " );";
-			eqs.push_back(string(eqj.c_str()));
-		}
-	}
-	for(int i=0;i<n;++i){
-		string eo;
-		for(int j=0;j<kn[i];++j){
-			eo+="x";
-			eo+=to_string(i+1);
-			eo+=to_string(j+1);
-			eo+=" + ";
+			}
 			
-			
-			sprintf(num, "r%d%d * x%d%d ;",i+1,j+1,i+1,j+1);
-			eqs.push_back(string(num));
-		}
-		eo+=" - 1;";
-		eqs.push_back(string(eo.c_str()));
-	}
-	for(int i=0;i<eqs.size();++i){
-		printf("%s\n", eqs[i].c_str());
-	}
-	
-	pstream phc; // for running phc
-	phc.open("./phc -b");
-	if(!phc.is_open())
-	{
-		printf("Could not open the phc pack executable.\n");
-	}
-	printf("n=%d\n",eqs.size());
-		// start inputting
-	phc << "n" << endl; // is system on input file? no
-	phc << num_eq << endl; // number of polynomials
-	phc << num_eq << endl; // number of unknowns
-	for(int i=0;i<eqs.size();++i){
-		phc << eqs[i].c_str()<<endl;
-	}
-	phc << "n" << endl;
-	string opstr;
-	vector<string> varname;
-	double* solution = (double*)malloc(num_eq*sizeof(double));
-	while(phc >> opstr)
-	{
-		if(opstr.compare("solution")==0){
-			phc >> opstr;
-			if(opstr.compare("for")==0){
-				varname.clear();
-				phc >> opstr;//t
-				phc >> opstr;//:
-				//phc >> opstr;
-				bool done=1;
-				for(int i=0;i<num_eq;++i){
-					string vname;
-					double reald,compd;
-					phc >> vname; //variable name
-					phc >> opstr; //: skip
-					phc >> reald;
-					phc >> compd;
-					if(reald<MZERO){
-						done=0;
-						//printf("%f\n",reald);
-						break;
-					}
-					if(compd<MZERO||compd>PZERO){
-					
-						done=0;
-						//printf("%f\n",compd);
-						break;
-					}
-					solution[i] = reald;
-					varname.push_back(vname);
-					
-				}
+			for(int j=1;j<kn[i];++j){
+				eqj.clear();
+				//using r
+				eqj += "r";
 				
-				if(done){
-					printf("solution:\n");
+				eqj+=to_string(i+1);
+				
+				eqj+=to_string(j+1);
+				//using r
+				for(int k=0;k<dimension[n]/kn[i];++k) {
+					int smallk =k%dimension[i];
+					int curk = (k-smallk)*kn[i] + smallk + dimension[i]*j;
+					int idc = 0;
+					if(util[curk][i]>=0){//k=1
+						eqj+=" + ";
+					}
+					
+					eqj+=to_string(util[curk][i]);
+					for(int w=n-1;w>=0;--w){
+						currentIndex[w] = curk/dimension[w];
+						curk = curk%dimension[w];
+						if(w==i)continue;
+						eqj+=" * ";
+						eqj+="x";
+						
+						eqj+=to_string(w+1);
+						
+						eqj+=to_string(currentIndex[w]+1);;
+						
+					}
+					
+				}	
+				eqj += " - ( "+ eq0 + " );";
+				eqs.push_back(string(eqj.c_str()));
+			}
+		}
+		for(int i=0;i<n;++i){
+			string eo;
+			for(int j=0;j<kn[i];++j){
+				eo+="x";
+				eo+=to_string(i+1);
+				eo+=to_string(j+1);
+				eo+=" + ";
+				
+				
+				sprintf(num, "r%d%d * x%d%d ;",i+1,j+1,i+1,j+1);
+				eqs.push_back(string(num));
+			}
+			eo+=" - 1;";
+			eqs.push_back(string(eo.c_str()));
+		}
+		for(int i=0;i<eqs.size();++i){
+			printf("%s\n", eqs[i].c_str());
+		}
+		
+		pstream phc; // for running phc
+		phc.open("./phc -b");
+		if(!phc.is_open())
+		{
+			printf("Could not open the phc pack executable.\n");
+		}
+		printf("n=%d\n",eqs.size());
+			// start inputting
+		phc << "n" << endl; // is system on input file? no
+		phc << num_eq << endl; // number of polynomials
+		phc << num_eq << endl; // number of unknowns
+		for(int i=0;i<eqs.size();++i){
+			phc << eqs[i].c_str()<<endl;
+		}
+		phc << "n" << endl;
+		string opstr;
+		vector<string> varname;
+		double* solution = (double*)malloc(num_eq*sizeof(double));
+		while(phc >> opstr)
+		{
+			if(opstr.compare("solution")==0){
+				phc >> opstr;
+				if(opstr.compare("for")==0){
+					varname.clear();
+					phc >> opstr;//t
+					phc >> opstr;//:
+					//phc >> opstr;
+					bool done=1;
 					for(int i=0;i<num_eq;++i){
-						printf("%s: %f\n",varname[i].c_str(),solution[i]);
+						string vname;
+						double reald,compd;
+						phc >> vname; //variable name
+						phc >> opstr; //: skip
+						phc >> reald;
+						phc >> compd;
+						if(reald<MZERO){
+							done=0;
+							//printf("%f\n",reald);
+							break;
+						}
+						if(compd<MZERO||compd>PZERO){
+						
+							done=0;
+							//printf("%f\n",compd);
+							break;
+						}
+						solution[i] = reald;
+						varname.push_back(vname);
+						
+					}
+					
+					if(done){
+						printf("solution:\n");
+						for(int i=0;i<num_eq;++i){
+							printf("%s: %f\n",varname[i].c_str(),solution[i]);
+						}
 					}
 				}
 			}
+			
 		}
+		phc.clear();
+		phc.close();
+		
 		
 	}
-	phc.clear();
-	phc.close();
 	gettimeofday(&endTime, NULL);
 	totalTime =  (endTime.tv_sec - startTime.tv_sec) * 1000000L;
     totalTime += (endTime.tv_usec - startTime.tv_usec);
